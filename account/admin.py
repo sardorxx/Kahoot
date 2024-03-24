fef2439795f0df023d4519cc0f5a763db26100e6
Sardor <xalilovsardor4605@gmail.com>
2024-03-24 12:12:25 +0500

    db changes

--- account/admin.py
+++ account/admin.py
@@ -5,7 +5,7 @@ from account.models import CustomUser
 # Register your models here.
 
 class AccountAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'user_type')
+    list_display = ('full_name', 'email', 'user_type')
 
 
 admin.site.register(CustomUser, AccountAdmin)
--- account/models.py
+++ account/models.py
@@ -34,7 +34,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         (OPTION3, 'Admin'),
     ]
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=20, unique=True)
+    username = models.CharField(max_length=20, null=True, blank=True)
     full_name = models.CharField(max_length=30)
     image = models.ImageField(upload_to='users_image/')
     user_type = models.CharField(
@@ -51,7 +51,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username','user_type']
+    REQUIRED_FIELDS = ['full_name', 'user_type']
 
     def __str__(self):
         return self.username
