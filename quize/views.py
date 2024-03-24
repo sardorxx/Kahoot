e2a88236ff15292fdd82011c6a245851d5943117
Sardor <xalilovsardor4605@gmail.com>
2024-03-24 14:15:14 +0500

    validate

--- quize/views.py
+++ quize/views.py
@@ -14,19 +14,10 @@ class TeacherSubjectList(generics.CreateAPIView):
     queryset = Teacher_Subject.objects.all()
     serializer_class = TeacherSubjectSerializer
 
-    def post(self, request):
+    def create_subject(self, request):
         serializer = TeacherSubjectSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def validate(self, data):
-        queryset = TeacherSubjectSerializer.objects.get(
-            Q(user_type=data['user_type']) & Q(user_email=data['user_email']))
-        for teacher in queryset:
-            if teacher.subject == data['subject']:
-                raise serializers.ValidationError('Subject Already Exists')
-        return queryset
-# asa
