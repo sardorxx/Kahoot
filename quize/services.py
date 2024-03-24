d30e60623577c21f0d3e971a09e6616ccab1ab45
Sardor <xalilovsardor4605@gmail.com>
2024-03-24 16:18:30 +0500

    validate updated

--- quize/services.py
+++ quize/services.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
+
+from account.models import CustomUser
 from quize.models import Teacher_Subject
+
 
 def validate_unique_subject(data):
     """
@@ -7,9 +10,12 @@ def validate_unique_subject(data):
     """
     user_email = data['user_email']
     subject = data['subject']
+    user = CustomUser.objects.get(email=user_email)
 
-    if Teacher_Subject.objects.filter(subject=subject, user_email=user_email).exists():
+    if (not Teacher_Subject.objects.filter(subject=subject, user_email=user_email).exists() and
+            user.user_type == "Teacher"):
+        pass
+    elif user.user_type != "Teacher":
+        raise serializers.ValidationError("You are not teacher")
+    else:
         raise serializers.ValidationError("This subject already exists for this user.")
-
-
-
