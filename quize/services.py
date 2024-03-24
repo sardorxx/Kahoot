24400ec7b392a9652187729dd26c401e2356058f
Husniddin007 <husniddingofforov99@gmail.com>
2024-03-24 15:58:35 +0500

    update databases

--- quize/services.py
+++ quize/services.py
@@ -1,15 +1,15 @@
-from django.db.models import Q
-from rest_framework import validators, serializers
-
+from rest_framework import serializers
 from quize.models import Teacher_Subject
-from quize.serializers import TeacherSubjectSerializer
 
+def validate_unique_subject(data):
+    """
+    Validate that the subject is unique for each user_email.
+    """
+    user_email = data['user_email']
+    subject = data['subject']
 
-# def validate_teacher_subject(data):
-#     queryset = Teacher_Subject.objects.get(Q(user_type=data['user_type']) & Q(user_email=data['user_email'])).all()
-#     for teacher in queryset:
-#         if teacher.subject == data['subject']:
-#             raise serializers.ValidationError('Subject Already Exists')
-#     return data
+    if Teacher_Subject.objects.filter(subject=subject, user_email=user_email).exists():
+        raise serializers.ValidationError("This subject already exists for this user.")
+
 
 
