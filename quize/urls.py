d6c33ecc06740322b5f515e1f70fcbc2b80dadbd
Husniddin007 <husniddingofforov99@gmail.com>
2024-03-25 14:53:43 +0500

    add question functions

--- quize/urls.py
+++ quize/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from quize.views import TeacherSubjectList
+from quize.views import (TeacherSubjectList,
+                         QuestionSetAPIView,
+                         AddQuestionAPIView)
 
 urlpatterns = [
     path('', TeacherSubjectList.as_view(), name='teacher-subject-list'),
+    path('add_question_set/', QuestionSetAPIView.as_view(), name='add-question-set'),
+    path('add_question/', AddQuestionAPIView.as_view(), name='add-question'),
 ]
--- quize/views.py
+++ quize/views.py
@@ -1,12 +1,11 @@
-
 from django.db.models import Q
 from django.shortcuts import render
 from rest_framework import generics, status, serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from quize.models import Teacher_Subject
-from quize.serializers import TeacherSubjectSerializer
+from quize.models import Teacher_Subject, Question_Set,Question
+from quize.serializers import TeacherSubjectSerializer, QuestionSetSerializer, QuestionSerializer
 
 
 # Create your views here.
@@ -21,3 +20,26 @@ class TeacherSubjectList(generics.CreateAPIView):
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class QuestionSetAPIView(generics.CreateAPIView):
+    queryset = Question_Set.objects.all()
+    serializer_class = QuestionSetSerializer
+
+    def add_question_set(self, request):
+        serializer = QuestionSetSerializer(data=request.data, files=None)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AddQuestionAPIView(generics.CreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+    def add_question(self, request):
+        serializer = QuestionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
