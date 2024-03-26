f9b6a039d98588c79a74b2c18b0e5396fa5a36a9
Sardor <xalilovsardor4605@gmail.com>
2024-03-26 15:57:05 +0500

    get question set

--- quize/urls.py
+++ quize/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from quize.views import (TeacherSubjectList,
                          QuestionSetAPIView,
-                         AddQuestionAPIView)
-
+                         AddQuestionAPIView,
+                         QuestionSetListAPIView)
 urlpatterns = [
     path('add_subject/', TeacherSubjectList.as_view(), name='teacher-subject-list'),
     path('add_question_set/', QuestionSetAPIView.as_view(), name='add-question-set'),
     path('add_question/', AddQuestionAPIView.as_view(), name='add-question'),
+    path('get_question_set/', QuestionSetListAPIView.as_view(), name='question-set-list'),
 ]
--- quize/views.py
+++ quize/views.py
@@ -1,5 +1,7 @@
-from rest_framework import generics, status
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, status, viewsets
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from quize.models import Teacher_Subject, Question_Set, Question, Answer
 from quize.serializers import TeacherSubjectSerializer, QuestionSetSerializer, QuestionSerializer
@@ -43,5 +45,8 @@ class AddQuestionAPIView(generics.CreateAPIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class AddAnswerAPIView(generics.CreateAPIView):
-    queryset = Answer.objects.filter()
+class QuestionSetListAPIView(generics.ListAPIView):
+    queryset = Question_Set.objects.all()
+    serializer_class = QuestionSetSerializer
+
+
